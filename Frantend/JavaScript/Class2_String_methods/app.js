5cb0a0b64bb466251c48cfba4d0e8c2f95c5bc73
Parthrajput-git <pr9410350@gmail.com>
2025-09-07 16:45:07 +0530

    Add files via upload

---
+++ Frantend/JavaScript/Class2_String_methods/app.js
@@ -0,0 +1,28 @@
+//String methods
+//(01) slice
+let stri="  HelloWorld  ";
+//(01.1)
+console.log(stri.slice(0,5));
+//(01.2)
+console.log(stri.slice(5,stri.length));
+//(01.3)
+console.log(stri.slice(-3));//10-3=7
+
+//(02)
+console.log(stri.toLowerCase());
+
+//(03)
+console.log(stri.toUpperCase());
+
+//(04)
+let res= "   Bye"
+console.log(res.trim());
+
+//(05)
+console.log(stri.replace("ll","ld"));
+console.log(stri);//String is not mutable
+
+// (06)
+console.log(stri.repeat(4));
+
+
