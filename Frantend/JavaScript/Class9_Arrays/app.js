e30f0454c297bf194aa3d05b2b011052350b2677
Parthrajput-git <pr9410350@gmail.com>
2025-09-17 23:39:20 +0530

    Update app.js

--- Frantend/JavaScript/Class9_Arrays/app.js
+++ Frantend/JavaScript/Class9_Arrays/app.js
@@ -7,4 +7,86 @@
 let arr=[1,2,3,4,5];
 arr.splice(3);
 console.log(arr);
+// // Array important methods 
+// // push () add to end
 
+let arr0=[ "Ram", "Purva" ,"Raghav", "Megha" ,"ved"];
+
+let ar=[ "Student1", "Student2"];
+
+arr.push("Prachi");
+console.log(arr);
+
+// Unshift()  add to start
+
+arr.unshift("Vidhi");
+console.log(arr);
+
+// // pop() delete end and return it
+arr.pop();
+console.log(arr);
+
+// shift() delete start and return it
+arr.shift();
+console.log(arr);
+
+// // concat arrays
+ console.log(arr1.concat(arr));
+
+
+// // reverse()
+console.log(arr.reverse());
+
+// slice()
+console.log(arr.slice(2));
+ 
+console.log(arr.slice(2,4));
+
+//splice()
+//// Q.1 
+
+let  arr4= ['January','July','March','august','student1','student2'];
+
+console.log(arr.shift());
+console.log(arr.shift());
+console.log(arr.unshift('June'));
+console.log(arr.unshift('july'));
+console.log(arr);
+
+// indexof() and includes
+
+console.log(arr.indexOf("august"));
+
+console.log(arr.includes("March"));
+
+console.log(arr.reverse());
+
+
+console.log(arr.slice(2));
+
+console.log(arr.slice(1,2));
+
+console.log(arr);
+
+console.log(arr.splice(4));
+console.log(arr.splice(1,4));
+
+console.log(arr.splice(0,2,2));
+console.log(arr);
+
+
+let  arr= ['January','July','March','august','student1','student2'];
+
+//// sort  method in array
+
+let arr1=[1,3,4,2,6,4,5,8,0];
+console.log(arr1.sort());
+
+let arr2=['a','b','d','c','f','d'];
+console.log(arr2.sort());
+
+console.log(arr.splice(0,2,"july","june"));
+console.log(arr);
+
+console.log(arr.reverse().indexOf('March'));
+console.log(arr);
