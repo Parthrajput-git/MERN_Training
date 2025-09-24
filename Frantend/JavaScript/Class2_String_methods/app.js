b1ac8a7fe1eafa8d52120086205e47d4f2ffc6a8
Parthrajput-git <pr9410350@gmail.com>
2025-09-24 11:30:57 +0530

    Update app.js

--- Frantend/JavaScript/Class2_String_methods/app.js
+++ Frantend/JavaScript/Class2_String_methods/app.js
@@ -25,4 +25,19 @@ console.log(stri);//String is not mutable
 // (06)
 console.log(stri.repeat(4));
 
+// Freeze method
+
+const obj = {
+    fname : "Rohan",
+      age : 80
+}
+console.log("Original  object is",obj);
+obj.lname="Mahi";
+
+Object.freeze(obj);
+obj.id="23";
+console.log("After the  object is",obj);
+
+
+
 
