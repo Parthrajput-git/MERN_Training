c67064516fec70b1791453e769aa2ee7164daf9d
Parthrajput-git <pr9410350@gmail.com>
2025-09-28 17:21:37 +0530

    Update script.js

--- Frantend/JavaScript/Class9_Dom/script.js
+++ Frantend/JavaScript/Class9_Dom/script.js
@@ -31,41 +31,5 @@ let diiv=document.querySelector("div");
 diiv.append(btn);
 
 
-// part 2
-// // Selectors
-// let dir=document.getElementById(".box");
-
-// console.log(dir);
-
-// let man=document.querySelector(".box2");
-
-// console.log(man);
-
-// // getAttribute selector
-// let div=document.querySelector("div");
-// let id=div.getAttribute("id");
-// console.log(id);
-
-// let para=document.querySelector("p");
-// let p=para.setAttribute("Para","written")
-
-// console.log(p);
-
-
-// // node.style
-// let divn=document.querySelector("div");
-// let pri=divn.style.backgroundColor="red";
-
-// Insart Elements
-let btn=document.createElement("button");
-btn.innerText="clik me";
-console.log(btn);
-
-
-let diiv=document.querySelector("div");
-diiv.append(btn);
-
-
-
 
 
