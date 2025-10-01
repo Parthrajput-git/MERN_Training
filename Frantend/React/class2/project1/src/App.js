00a7bd16bb93cda820586e0f71c3c4669163efbc
Parthrajput-git <pr9410350@gmail.com>
2025-10-01 16:39:37 +0530

    Class2

---
+++ Frantend/React/class2/project1/src/App.js
@@ -0,0 +1,14 @@
+import Data from "./components/Data";
+
+function App(){
+  
+         
+  return(
+    <div>This is app
+       <Data>This is</Data>
+      <Data/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Frantend/React/class2/project1/src/components/Data.js
@@ -0,0 +1,5 @@
+function Data(){
+    return(<div>This is a  data components</div>)
+}
+
+export default Data;
