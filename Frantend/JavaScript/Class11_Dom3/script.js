445347124c25a3a69b94609994731b4673e753df
Parthrajput-git <pr9410350@gmail.com>
2025-10-01 12:26:50 +0530

    Update script.js

--- Frantend/JavaScript/Class11_Dom3/script.js
+++ Frantend/JavaScript/Class11_Dom3/script.js
@@ -12,3 +12,23 @@ h3.innerText="i'm a blue h3!";
 h3.style.color="blue";
 
 body.append(h3);
+
+let div=document.createElement("div");
+
+div.style.border="2px solid black";
+div.style.backgroundColor="pink"
+div.innerText="Div"
+body.append(div);
+
+let pa=document.createElement("p");
+pa.innerText="ME TOO!";
+div.append(pa);
+
+let h1=document.createElement("h1");
+h1.innerText="I'm in a div";
+div.append(h1);
+
+pa.insertAdjacentElement('beforebegin',h1);
+
+
+
