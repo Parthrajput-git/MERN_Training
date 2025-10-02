7d3a8575861fed445d2b5e03cff357feab5f0334
Parthrajput-git <pr9410350@gmail.com>
2025-10-02 22:40:07 +0530

    Update script.js

--- Frantend/JavaScript/Class11_Dom3/script.js
+++ Frantend/JavaScript/Class11_Dom3/script.js
@@ -73,6 +73,7 @@ function value() {
 }
 btn.onmouseenter=value;
 
+//Practice Dom
 let body=document.querySelector("body");
 let btn=document.createElement("button");
 btn.innerText="Click me";
@@ -82,6 +83,7 @@ btn.addEventListener("click",()=>{
 })
 
 body.append(btn);
+
 
 
 
