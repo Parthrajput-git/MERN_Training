8043c74a929967892c65212a7775cb7b8a6aa58f
Parthrajput-git <pr9410350@gmail.com>
2025-10-03 13:27:23 +0530

    Add files via upload

---
+++ Frantend/Class13_API/async/script.js
@@ -0,0 +1,17 @@
+function wait() {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            let num = Math.floor(Math.random() * 10) + 1;
+            console.log(num);
+            res();
+
+        }, 1000);
+    });
+}
+
+async function op() {
+    await wait();
+    await wait();
+    wait();
+}
+op();
