349ab59ecbb31352ae4c65327a042d9ec814c286
Parthrajput-git <pr9410350@gmail.com>
2025-10-04 18:01:18 +0530

    Update script.js

---
+++ Frantend/JavaScript/Class13_API/async/API_Calling/script.js
@@ -0,0 +1,24 @@
+
+const div = document.querySelector("#mainbox");
+console.log(div);
+
+
+const url = "https://fakestoreapi.com/products/";
+
+async function apiCall() {
+    const data = await fetch(url);
+    const result = await data.json();
+
+    result.map((item) => {
+        div.innerHTML += `<div class="boxdiv">
+        <p class="im"><img src="${item.image}"</p>
+        <p class="im">${item.category}</p>
+        <p> ${item.price}</p>
+
+       </div>`
+
+    });
+
+    console.log(result);
+}
+apiCall();
