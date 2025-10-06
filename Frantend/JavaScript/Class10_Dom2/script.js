0f631be8816d31588605511b4c4406c0433878c8
Parthrajput-git <pr9410350@gmail.com>
2025-10-06 17:09:31 +0530

    Update script.js

--- Frantend/JavaScript/Class10_Dom2/script.js
+++ Frantend/JavaScript/Class10_Dom2/script.js
@@ -1,7 +1,7 @@
 // Selectors
 let dir=document.getElementById(".box");
 
-console.log(dir);
+console.log(dir)
 
 let man=document.querySelector(".box2");
 
@@ -161,6 +161,7 @@ document.querySelector("body").append(button);
 
 let parag=document.querySelector("p");
 parag.classList.add("newClass");
+
 
 
 
