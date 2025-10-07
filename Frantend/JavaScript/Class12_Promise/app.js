5ca794ca3c6cfea0bcc33ce4a0d100282324e644
Parthrajput-git <pr9410350@gmail.com>
2025-10-07 21:47:38 +0530

    Update app.js

--- Frantend/JavaScript/Class12_Promise/app.js
+++ Frantend/JavaScript/Class12_Promise/app.js
@@ -2,7 +2,7 @@
 let promise = new Promise((resolve,reject)=>{
     let age = Math.floor((Math.random(0,10)*100));
     if(age>=18){
-        console.log("able to voting")     
+        console.log("able to voting");    
     }
     else{
         console.log("rejected");
