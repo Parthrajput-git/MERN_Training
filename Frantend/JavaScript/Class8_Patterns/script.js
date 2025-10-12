5f7eb67713ac2719c8ee70f322a76d132d8a3525
Parthrajput-git <pr9410350@gmail.com>
2025-10-12 21:30:40 +0530

    Update script.js

--- Frantend/JavaScript/Class8_Patterns/script.js
+++ Frantend/JavaScript/Class8_Patterns/script.js
@@ -100,20 +100,20 @@ for(let i=1;i<=5;i++){
 console.log(emty);
 }
 
-// // 1
-// // 2 3
-// // 4 5 6
-// // 7 8 9 10
-// let count=1;
-// for(let i=1;i<=5;i++){
-//     let emty="";
-//     for(let j=1;j<=i;j++){
-//        emty+=count+" ";
-//        count++;
-//     }
-//     console.log(emty);
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+let count=1;
+for(let i=1;i<=5;i++){
+    let emty="";
+    for(let j=1;j<=i;j++){
+       emty+=count+" ";
+       count++;
+    }
+    console.log(emty);
     
-// }
+}
 
 // 1
 // 0 1
