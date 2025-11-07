ccfb2cc925f5ca253a8dde066af22b82a9701f08
@Parth Rajput <pr9410350@gmail.com>
2025-11-07 22:45:04 +0530

    Update script.js

--- Frantend/JavaScript/Class13_API/async/API_Calling/script.js
+++ Frantend/JavaScript/Class13_API/async/API_Calling/script.js
@@ -1,6 +1,6 @@
 // APL calling help with DOM.
-const div = document.querySelector("#mainbox");
-console.log(div);
+// const div = document.querySelector("#mainbox");
+// console.log(div);
 
 
 const url = "https://fakestoreapi.com/products/";
@@ -21,6 +21,7 @@ async function apiCall() {
     console.log(result);
 }
 apiCall();
+
 
 
 
