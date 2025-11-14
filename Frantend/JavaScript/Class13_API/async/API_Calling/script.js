2a3a3c90b1e8f845460ae037aecf9e1cf4ee02e8
@Parth Rajput <pr9410350@gmail.com>
2025-11-14 14:42:36 +0530

    Update script.js

--- Frantend/JavaScript/Class13_API/async/API_Calling/script.js
+++ Frantend/JavaScript/Class13_API/async/API_Calling/script.js
@@ -5,7 +5,7 @@ console.log(div);
 
 const url = "https://fakestoreapi.com/products/";
 
-// async function apiCall() {
+async function apiCall() {
     const data = await fetch(url);
     // const result = await data.json();
 
@@ -21,6 +21,7 @@ const url = "https://fakestoreapi.com/products/";
     console.log(result);
 }
 apiCall();
+
 
 
 
