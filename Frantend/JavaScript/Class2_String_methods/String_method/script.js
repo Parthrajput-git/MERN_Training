cbe0fa69de3448afdd620aa1b2c2cd90633297d9
@Parth Rajput <pr9410350@gmail.com>
2026-01-19 17:41:20 +0530

    Add files via upload

---
+++ Frantend/JavaScript/Class2_String_methods/String_method/script.js
@@ -0,0 +1,35 @@
+// let h="abdcs";
+// let n=h.toUpperCase();
+// console.log(n);
+let h=[1,3,4,5,6,7];
+// let n=h.toLowerCase();
+// console.log(n);
+
+// console.log(h.indexOf("s"));
+// p=h.reverse();
+// console.log(p);
+
+console.log(h.slice(1));
+
+function time(){
+    console.log("Hello my team  :");
+    setTimeout(()=>{
+        console.log("Not working Time  :");
+        
+    },2000);
+    console.log("How are you Bye!");
+    
+    
+}
+// time();
+function mine(){
+    console.log("Hello my team  :");
+    setInterval(()=>{
+        console.log("Not working Time  :");
+        
+    },2000);
+    console.log("How are you Bye!");
+    
+    
+}
+// mine();
