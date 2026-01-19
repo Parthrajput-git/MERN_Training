ece1ad0a9347d6f635db193a4f28109b5a513826
@Parth Rajput <pr9410350@gmail.com>
2026-01-19 17:37:27 +0530

    Add files via upload

---
+++ Frantend/JavaScript/Class13_API/async/Class2_Api/script2.js
@@ -0,0 +1,20 @@
+let btn = document.querySelector("button");
+let url2 = "https://dog.ceo/api/breeds/image/random";
+btn.addEventListener("click", async () => {
+    let link = await getDog();
+    let result = document.querySelector("#im")
+    result.setAttribute("src", link);
+    console.log(link);
+
+
+});
+async function getDog() {
+    try {
+        let res = await axios.get(url2);
+        return res.data.message;
+    } catch (e) {
+        console.log("Error === :", e);
+        return "Not found ";
+    }
+
+}
