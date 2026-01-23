c1f4f478b979cfe93c95bb36601bb28bc2a033d9
@Parth Rajput <pr9410350@gmail.com>
2026-01-23 22:23:52 +0530

    Add files via upload

---
+++ Backend/EJSDir/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+let port = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+});
+
+
+app.get("/ig/:username", (req, res) => {
+    let followers=["Pop","Rem","Som","Mona","Joy"];
+    let { username } = req.params;
+    res.render("instagram.ejs",{username,followers})
+
+})
+
+
+app.get("/roll", (req, res) => {
+    let disVal = Math.floor(Math.random() * 6) + 1;
+    res.render("home.ejs", { disVal })
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Port are listening ${port}`);
+
+})
