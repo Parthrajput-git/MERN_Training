cb8f39f483740f56304d0bf6be828999924f2581
@Parth Rajput <pr9410350@gmail.com>
2026-01-26 22:30:09 +0530

    Add files via upload

---
+++ Backend/Miscellaneoust/Backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+let port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get("/register", (req, res) => {
+    let { user, password } = req.query;
+    res.send(`Standard GET response ${user}`);
+});
+
+app.post("/register", (req, res) => {
+    let { user, password } = req.body;
+    res.send(`Standard POST response, Welcome @${user}`);
+});
+
+app.listen(port, () => {
+    console.log("Port are listening :");
+
+});
