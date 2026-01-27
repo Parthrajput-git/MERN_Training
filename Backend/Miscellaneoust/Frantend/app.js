94d686277e051e22276a2e21aad7bc58b87295de
@Parth Rajput <pr9410350@gmail.com>
2026-01-27 16:58:11 +0530

    Update app.js

--- Backend/Miscellaneoust/Frantend/app.js
+++ Backend/Miscellaneoust/Frantend/app.js
@@ -8,6 +8,20 @@ let p1 = new Person("Eve", 25);
 console.log(p1);
 console.log(p1.name);
 
+//Classes
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+
+    }
+    talk() {
+        console.log(`Hello i name is ${this.name}`);
+    }
+}
+let p1= new Person("Mony",19);
+console.log(p1);
+console.log(p1.talk());
 
 
 
