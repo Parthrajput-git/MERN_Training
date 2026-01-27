d94576b62147a95ef5d0ab00ffc0c339d982fcb1
@Parth Rajput <pr9410350@gmail.com>
2026-01-27 17:07:58 +0530

    Update app.js

--- Backend/Miscellaneoust/Frantend/app.js
+++ Backend/Miscellaneoust/Frantend/app.js
@@ -23,6 +23,31 @@ let p1= new Person("Mony",19);
 console.log(p1);
 console.log(p1.talk());
 
+ //Inheritance
+class Person{
+    constructor(name,age){
+    this.name=name;
+    this.age=age;
+    }
+    talk(){
+        return "Hello";
+        
+    }
+}
+
+class Student extends Person{
+    constructor(name,age,marks){
+        super(name,age);
+        this.marks=marks;
+    }
+}
+
+let p1=new Student("Bory",21,89.05);
+console.log(p1);
+console.log("Student marks  :",p1.marks);
+console.log(p1.talk());
+
+
 
 
 
