33f3e5458c3a6b51f19b8936f98afd1de282004a
@Parth Rajput <pr9410350@gmail.com>
2026-01-29 23:39:29 +0530

    Update index.js

--- Backend/RESTDir/Class1/index.js
+++ Backend/RESTDir/Class1/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 let port = 3000;
 const path = require("path");
-const{ v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
+const methodOverride = require("method-override");
 
 
-
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -38,7 +40,7 @@ app.get("/posts/new", (req, res) => {
 app.post("/posts", (req, res) => {
     let { username, content } = req.body;
     let id = uuidv4();
-    posts.push({ id , username, content })
+    posts.push({ id, username, content })
     res.redirect("/posts");
 
 });
@@ -48,21 +50,35 @@ app.get("/posts", (req, res) => {
 
 app.get("/posts/:id", (req, res) => {
     let { id } = req.params;
+    console.log(id);
     let post = posts.find((p) => id === p.id);
-    res.render("show.ejs",{ post })
+    res.render("show.ejs", { post })
 });
 
-// app.patch("/posts/:id",(req,res)=>{
-//     let { id } = req.params;
-//     let newContent = req.body.content;
-//     let post = posts.find((p) => id === p.id);
-//     post.content = newContent;
-//     console.log(post);
-//     res.send(" Patch request are working ");
-    
-// });
+app.patch("/posts/:id", (req, res) => {
+    console.log("REQ.BODY =", req.body);
+    let { id } = req.params;
+    let newContent = req.body.content;
+
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
+    console.log(post);
+    res.redirect("/posts");
+
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs", { post });
+});
+
+app.delete("/posts/:id",(req,res)=>{
+      let { id } = req.params;
+    posts = posts.filter((p) => id !== p.id);
+     res.redirect("/posts");
+});
 
 app.listen(port, () => {
-    console.log("Port are listening ");
+    console.log("listening are port : 3000 ");
 });
-
