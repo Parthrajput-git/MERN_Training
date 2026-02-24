a6709572ebafeb12ed8be762ca1a3be405f895c4
@Parth Rajput <pr9410350@gmail.com>
2026-02-24 23:46:41 +0530

    Update index.js

--- Backend/Middlewares/index.js
+++ Backend/Middlewares/index.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
     next();
 });
 
-
+//Condition
 const checkToken=("/api", (req, res, next) => {
     let {token}=req.query;
     if(token==="access"){
@@ -66,6 +66,7 @@ app.listen(8080, () => {
 
 
 });
+
 
 
 
