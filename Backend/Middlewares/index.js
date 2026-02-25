b8f2e8cda265fea106e7bf1c2cdc67c4274b01f8
@Parth Rajput <pr9410350@gmail.com>
2026-02-25 22:14:04 +0530

    Update index.js

--- Backend/Middlewares/index.js
+++ Backend/Middlewares/index.js
@@ -20,7 +20,7 @@ const app = express();
 
 app.use((req, res, next) => {
     let time = new Date();
-    console.log(req.method, req.path, req.hostname, req, time);
+    console.log(req.method, req.path, req.hostname, req, time);// This concept is reqest propertice
     next();
 });
 
@@ -67,6 +67,7 @@ app.listen(8080, () => {
 
 
 });
+
 
 
 
