bbedfcb36749373dec065dc03d50b2fa961f89d5
@Parth Shingh Rajput <pr9410350@gmail.com>
2026-03-27 23:29:46 +0530

    Update index.js

--- Backend/EJSDir/index.js
+++ Backend/EJSDir/index.js
@@ -2,7 +2,8 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-let port = 3000; // port server
+
+let port = 3000; 
 
 app.use(express.static(path.join(__dirname,"public")));
 app.set("view engine", "ejs");
